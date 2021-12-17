a2ea20e73d956350cd608c4604c0757fdae660c4
AedenMoylan <aedenmoylan@gmail.com>
2021-12-17 13:42:37 +0000

    adding finished project

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, session
 
-from data_utils import save_the_data, process_data
-from datetime import datetime
+from data_utils import save_the_data, process_data, save_the_logs, process_logs
+import datetime 
 
 import random
 from collections import Counter
 
+
+
 app = Flask(__name__)
+
+sourceWord = ""
+guessWords = ""   
 
 @app.get("/")
 @app.get("/rules")
@@ -18,33 +23,108 @@ def show_rules():
 
 @app.get("/game")
 def game_play():
-
-    sourceword = get_source_word()
-
-    game_core(sourceword)
+    setup_txt_file()
+    srcwrd = get_source_word()
+    global sourceWord
+    sourceWord = srcwrd
 
     return render_template(
-        "game.html", title="Gameplay", source_word = sourceword
+        "game.html", title="Gameplay", source_word = sourceWord
     )  
 
 
 
+    
 @app.get("/checkResults")
 def check_results():
 
-    win = 0
+    global win
+    gsswrds = request.args.get("playerinput").split(" ")  
+ 
+    guessWords = gsswrds
 
+    #guessWords = "cars vail vails nails nail snail lava"
+
+    #guess = guessWords.split(" ")
+
+    for i in guessWords:
+        isCorrect = isWordInBigWord(i, sourceWord)
+        if(isCorrect == False):
+            tempThing = i
+            break
+
+    isWordTheSame = False
+    for i in guessWords:
+        if i == sourceWord:
+            isWordTheSame = True
+    
+    for i in guessWords:
+        isWordReused = isWordUsedMoreThanOnce(i, guessWords)
+        if isWordReused == True:
+             break
+
+        
+    win = False
+    if len(guessWords) == 5:
+        if isCorrect == True:
+            #if isWordReused == False:
+            if isWordTheSame == False:
+                win = True
+    print ()
 
     return render_template(
         "checkResults.html", title="Results", didWin=win
     )  
         
 
+@app.get("/saveScore")
+def saveTheScore():
 
-def isWordInBigWord(_word):
-    bigWord = Counter(wordToGuess)
+    playerName = request.args.get("playerName")  
+    time = datetime.datetime.now()
+    position = 1
+
+    score = 2
+    ##date_played =120
+    players_browser = request.user_agent.string
+    players_ip = request.environ.get('HTTP_X_REAL_IP',request.remote_addr)
+
+    
+
+
+    if win == True:
+        save_the_data(playerName,time,sourceWord, guessWords,position)
+        process_data()
+
+    save_the_logs(playerName,players_browser,players_ip,guessWords, sourceWord)
+    process_logs()
+    database = []     
+    database = process_data()     
+    print(database)
+
+    return render_template(
+        "saveScore.html", title="Score saved", data_base= database
+    )
+    
+
+@app.get("/logs")
+def processLogs():
+    logbase = []
+    logbase = process_logs()
+    print(logbase)
+    return render_template(
+        "logs.html",
+        data_base=logbase
+    )
+
+
+
+
+
+def isWordInBigWord(_word, _sourceWord):
+    bigWord = Counter(_sourceWord)
     smallWord = Counter(_word)
-    isCorrect = False
+   #isCorrect = False
     _word = smallWord - bigWord
     if bool(_word) == True:
         isCorrect = False
@@ -56,81 +136,35 @@ def isWordInBigWord(_word):
 
 
 
-def game_core(sourceword):
-    mainWord = Counter(sourceword)
-    guesswordList = []
-    wordWrong = False
-    wordRight = False
-    wordMistake = False
-    wordShort = False
-    wordEqualToSource = False
-
-
-    starttime = datetime.datetime.now()
-    for guesses in range(7):                           
-        guessWord = input("Enter Words:")              # Creates a string that will store the guessWord
-        guesswordList.append(guessWord)                # Adds to the end of the list which contains all the guess words
-        guess = Counter(guessWord)                     # Creates a counter which counts all the letters to check against the source word
-        guess = guess - mainWord                       # Checks if all the letters inside the source word, match the letters inside the guess word
-        mainWord = Counter(sourceword)                 # Reassigns the source word to main word, so you can check the next word
-        
-        if sourceword == guessWord:
-            wordEqualToSource = True
-        
-        if len(guessWord) >= 4:
-            with open("final.txt", "r") as sf:
-                for line in sf:
-                    stripped_line = line.strip()
-                    if stripped_line == guessWord:
-                        wordRight = True
-                    else:
-                        print("One or more of your words are not correct")
-                        wordRight = False
-
-            if wordRight == False:
-                wordMistake = True
-                print("The word is not real")
-            if wordRight == True:
-                print("it is found")
-                wordRight = False
-                if bool(guess) == True: # Checks if the guess word is empty, and if it isn't, the letters don't match
-                    print("Your letters don't match")
-                    wordWrong = True ## this will currently break out of the loop, if the word does not match
-                    break
-                if bool(guess) == False: # Checks if the guess word is empty, and if it is, then the letters match
-                    print("You guessed correctly")
-        else:
-            wordShort = True
-        
+def isWordUsedMoreThanOnce(_word,guess):
+    tempGuessList = guess.copy()
+    wordCounter = 0
+    
+    
+    for words in tempGuessList:
+        if _word == words:
+            wordCounter = wordCounter + 1
+         #   tempGuessList[tempGuessList.index(_word) ].replace(_word,'0', 1)
+            if wordCounter > 1:
+                break
             
-    endtime = datetime.now()
+        if wordCounter > 1:
+            print('word is reused')
+            usedMoreThanOnce = True
+        else:
+            usedMoreThanOnce = False
 
-    resulttime = endtime - starttime
-    resulttime = str(resulttime)
-    print("You finished in" ,resulttime)
-
-    duplicates = checkDupes(guesswordList)
-
-    if duplicates == True:
-        print("You have a duplicate in your words")
-    if wordMistake == True:
-        print("One or more of your words are not correct")
-    if wordShort == True:
-        print("One or more words are less than 4 letters")
-    if wordEqualToSource == True:
-        print("One or more words are equal to the sourceword")
-
-
+            return wordCounter 
 
 
 def setup_txt_file():
-    with open('final.txt', "w") as fw:
+    with open('final.txt', "w") as ff:
         with open('small.txt',"w") as sf:
             with open("big.txt","w") as bf:
                 with open("words.txt") as wf:
                     for w in wf:
                         if "'s" not in w:
-                            print(w.strip().lower(), file = fw)
+                            print(w.strip().lower(), file = ff)
                             if len(w) > 7:
                                 print(w.strip().lower(), file = bf)
                             else: 
--- data_utils.py
+++ data_utils.py
@@ -2,36 +2,86 @@ import DBcm
 
 
 config = {
-    'host': '127.0.0.1',
-    'database': 'leaderboardDB',
-    'user': 'leaderuser',
-    'password': 'leaderpasswd',
+    'host': '192.168.1.5',
+    'database': 'wordgameDB',
+    'user': 'wordgameuser',
+    'password': 'wordgamepasswd',
 }
 
 
+tempTime = 1
 
+# def save_the_data(name, timeTaken, bigWord, smallWords, position):
+#     guessStr=" ".join(smallWords)
+#     SQL = """
+#         insert into wordgametable
+#         (name, time_taken, big_word, small_words, position)
+#         values
+#         (%s, %s, %s, %s, %s)
+#     """
+#     with DBcm.UseDatabase(config) as db:
+#         db.execute(SQL, (name, timeTaken, bigWord, guessStr, position))
 
-def save_the_data(name, score):
+def save_the_data(name,timeTaken,bigWord, smallWords,position):
+    guessStr=" ".join(smallWords)
     SQL = """
-        insert into board
-        (name, score)
+        insert into wordgametable
+        (name,time_taken,big_word,small_words,position)
         values
-        (%s, %s)
+        (%s,%s,%s,%s,%s)
     """
     with DBcm.UseDatabase(config) as db:
-        db.execute(SQL, (name, score))
+        db.execute(SQL,(name,timeTaken,bigWord, smallWords,position))
 
 
-def process_data(the_score, the_player):
+
+
+def save_the_logs(name,players_browser,players_ip,players_word, word_given):
+    guessStr=" ".join(players_word)
+    SQL = """
+        insert into aedensLogs
+        (name,players_browser,players_ip,players_words,word_given)
+        values
+        (%s,%s,%s,%s,%s)
+    """
+    with DBcm.UseDatabase(config) as db:
+        db.execute(SQL,(name,players_browser,players_ip,guessStr, word_given))
+
+
+
+def process_logs():
     with DBcm.UseDatabase(config) as db:
         SQL = """
-            select name, score
-            from board
-            order by score desc
+            select * from aedensLogs order by date_played desc
         """
         db.execute(SQL)
-        scores = db.fetchall()
-    where = scores.index((the_player, the_score)) + 1
-    how_many = len(scores)
+        logs = []
+        logs = db.fetchall()
 
-    return where, how_many, scores[:10]
+
+
+    return logs
+
+
+def process_data():
+    with DBcm.UseDatabase(config) as db:
+        SQL = """
+          select * from wordgametable order by position desc
+        """
+        db.execute(SQL)
+        scores = []
+        scores = db.fetchall()
+        non_null=lambda x : "  "
+        for row in SQL:
+            list=map(non_null,row)
+            next=tuple(list)
+            scores.append(next)
+
+
+
+
+    return scores[:10]
+
+
+
+    
